60b753773f1a118fd93c0d62c05b9da5543b662b
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-17 04:44:18 +0530

    [Vedhashni] Updated the file to handle custom exception

---
+++ MoodAnalyser/CustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    class CustomException : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            NULL_EXCEPTION, EMPTY_EXCEPTION
+        }
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
--- MoodAnalyser/MoodAnalyse.cs
+++ MoodAnalyser/MoodAnalyse.cs
@@ -19,7 +19,11 @@ namespace MoodAnalyser
         {
             try
             {
-                if (message.ToLower().Contains("happy"))
+                if (message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
+                }
+                else if (message.ToLower().Contains("happy"))
                 {
                     return "happy";
                 }
@@ -28,10 +32,11 @@ namespace MoodAnalyser
                     return "sad";
                 }
             }
-            catch (NullReferenceException)
+            catch (NullReferenceException ex)
             {
-                return "happy";
+                throw new CustomException(CustomException.ExceptionType.NULL_EXCEPTION, "Mood should not be null");
             }
         }
     }
 }
+
